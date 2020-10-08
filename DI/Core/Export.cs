7c7ace2519eb48d824e8cf6604a5f985112c3ac2
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-10-08 22:10:35 +0200

    Added Export class.

---
+++ DI/Core/Export.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI
+{
+    public class Export
+    {
+        #region Constructor
+
+        public Export(object instance, Type typeOfInstance)
+        {
+            Instance = instance;
+            Type = typeOfInstance;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Type Type { get; }
+
+        public object Instance { get; }
+
+        #endregion
+    }
+}
