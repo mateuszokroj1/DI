a02d1f3027d7ddf46c6f415a5e72a2a14703042e
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-10-08 19:52:52 +0200

    Added CompositionCollection and CompositionException.

---
+++ DI/Core/Collections/CompositionCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DI.Collections
+{
+    public sealed class CompositionCollection : IReadOnlyList<Composition>
+    {
+        #region Fields
+
+        private readonly List<Composition> list = new List<Composition>();
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <exception cref="IndexOutOfRangeException" />
+        public Composition this[int index] => this.list[index];
+
+        public int Count => this.list.Count;
+
+        #endregion
+
+        #region Methods
+
+        internal void Add(Composition composition)
+        {
+            if (!this.list.Contains(composition))
+                this.list.Add(composition);
+        }
+
+        internal void Remove(Composition composition)
+        {
+            if (composition != null)
+                this.list.Remove(composition);
+        }
+
+        public IEnumerator<Composition> GetEnumerator() => this.list.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.list.GetEnumerator();
+
+        #endregion
+    }
+}
---
+++ DI/Core/Exceptions/CompositionException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DI
+{
+    public class CompositionException : Exception
+    {
+        #region Constructors
+
+        public CompositionException(string message)
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public new string Message { get; }
+
+        #endregion
+    }
+}
