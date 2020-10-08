052f4aff155b0cce46a8139ecbf2ee13cd440c1e
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-10-08 19:51:44 +0200

    Added Composition class.

---
+++ DI/Core/Composition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DI.Collections;
+
+namespace DI
+{
+    public class Composition
+    {
+        #region Fields
+
+        private List<Export> exports = new List<Export>();
+
+        #endregion
+
+        #region Constructors
+
+        public Composition() { }
+
+        public Composition(Composition parrentComposition)
+        {
+            Parent = parrentComposition;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Composition Parent { get; }
+
+        public CompositionCollection Children { get; } = new CompositionCollection();
+
+        #endregion
+
+        #region Methods
+
+        public bool TryImport<TImport>(out TImport destination)
+            where TImport : class
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
