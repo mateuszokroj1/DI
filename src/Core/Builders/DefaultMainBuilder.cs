64546b72a60c586a996460909d636d10a7afdb18
Mateusz Okrój <mateuszokroj1@hotmail.com>
2022-03-13 01:15:58 +0100

    Added first builders.

---
+++ src/Core/Builders/DefaultMainBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using DI.Extensibility;
+
+namespace DI.Builders
+{
+    public abstract class DefaultMainBuilder
+    {
+        internal IList<Type> modules = new List<Type>();
+
+        public virtual DefaultMainBuilder Add<TModule>()
+            where TModule : IModule
+        {
+            var type = typeof(TModule);
+
+            if (modules.IndexOf(type) > -1)
+                throw new ArgumentException("Module is just registered.");
+
+            modules.Add(type);
+
+            return this;
+        }
+    }
+}
---
+++ src/Core/Extensibility/IModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI.Extensibility
+{
+    public interface IModule
+    {
+        void Configure();
+    }
+}
